88bf6e28edcae2167043c7f2a016a4f69ed0cffb
Roy <roy.manigley@gmail.com>
2021-07-25 18:15:19 +0200

    fixed error in auth config → redirect to keycloak works again

--- src/main/java/ch/bytecrowd/securitydemo/config/WebSecurityConfig.java
+++ src/main/java/ch/bytecrowd/securitydemo/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +13,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable() // is needed to support HTTP PUT, POST and DELETE
+            //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        //.and()
             .authorizeRequests()
                 // .antMatchers("/api/*").permitAll()
                 .anyRequest().authenticated()
-                .and()
+        .and()
             .logout()
-                .permitAll();
+                .permitAll()
+        .and()
+            .oauth2Login();
     }
 }
